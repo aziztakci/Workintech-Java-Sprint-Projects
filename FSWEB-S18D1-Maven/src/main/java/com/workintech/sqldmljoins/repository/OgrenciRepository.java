83676619d1618a3d8094e65b36a07ed55c5f3568
aziztakci <takciazizz@gmail.com>
2026-05-18 20:58:20 +0300

    FSWEB-S18D1 added

---
+++ FSWEB-S18D1-Maven/src/main/java/com/workintech/sqldmljoins/repository/OgrenciRepository.java
@@ -0,0 +1,63 @@
+package com.workintech.sqldmljoins.repository;
+
+import com.workintech.sqldmljoins.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface OgrenciRepository extends JpaRepository<Ogrenci, Long> {
+
+
+    //Kitap alan öğrencilerin öğrenci bilgilerini listeleyin..
+    String QUESTION_2 = "SELECT o.*\n" +
+            "FROM ogrenci o\n" +
+            "INNER JOIN islem i ON o.ogrno = i.ogrno;";
+    @Query(value = QUESTION_2, nativeQuery = true)
+    List<Ogrenci> findStudentsWithBook();
+
+
+    //Kitap almayan öğrencileri listeleyin.
+    String QUESTION_3 = "SELECT o.* FROM ogrenci o\n" +
+            "LEFT JOIN islem i ON o.ogrno = i.ogrno\n" +
+            "WHERE i.islemno IS NULL;";
+    @Query(value = QUESTION_3, nativeQuery = true)
+    List<Ogrenci> findStudentsWithNoBook();
+
+    //10A veya 10B sınıfındaki öğrencileri sınıf ve okuduğu kitap sayısını getirin.
+    String QUESTION_4 = "SELECT DISTINCT c.* FROM ogrenci AS c\n" +
+            "INNER JOIN islem AS i ON c.ogrno = i.ogrno\n" +
+            "WHERE sinif IN ('10A','10B')";
+    @Query(value = QUESTION_4, nativeQuery = true)
+    List<KitapCount> findClassesWithBookCount();
+
+    //Öğrenci tablosundaki öğrenci sayısını gösterin
+    String QUESTION_5 = "SELECT COUNT(*) FROM ogrenci ";
+    @Query(value = QUESTION_5, nativeQuery = true)
+    Integer findStudentCount();
+
+    //Öğrenci tablosunda kaç farklı isimde öğrenci olduğunu listeleyiniz.
+    String QUESTION_6 = "SELECT COUNT(DISTINCT ad) FROM ogrenci;";
+    @Query(value = QUESTION_6, nativeQuery = true)
+    Integer findUniqueStudentNameCount();
+
+    //--İsme göre öğrenci sayılarının adedini bulunuz.
+    //--Ali: 2, Mehmet: 3
+    String QUESTION_7 = "SELECT ad, COUNT(*) FROM ogrenci\n" +
+            "GROUP BY ad";
+    @Query(value = QUESTION_7, nativeQuery = true)
+    List<StudentNameCount> findStudentNameCount();
+
+
+    String QUESTION_8 = "SELECT sinif, COUNT(*) FROM ogrenci\n" +
+            "GROUP BY sinif";
+    @Query(value = QUESTION_8, nativeQuery = true)
+    List<StudentClassCount> findStudentClassCount();
+
+    String QUESTION_9 = "SELECT c.ad,c.soyad, COUNT(i.kitapno) FROM ogrenci AS c\n" +
+            "INNER JOIN islem AS i ON c.ogrno = i.ogrno\n" +
+            "GROUP BY c.ad,c.soyad\n" +
+            "ORDER BY COUNT(i.kitapno)";
+    @Query(value = QUESTION_9, nativeQuery = true)
+    List<StudentNameSurnameCount> findStudentNameSurnameCount();
+}
